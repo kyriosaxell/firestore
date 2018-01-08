9364bfb1b1dcc9fe46f8458ad6b6d70214b780c1
Axell <soad.dvm@hotmail.com>
2018-01-07 22:26:55 -0600

    Primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,12 @@
 import { Component } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+
+interface Post{
+  title: string;
+  content: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +14,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  postsCol: AngularFirestoreCollection<Post>;
+  posts: Observable<Post[]>;
+
+  constructor(private afs: AngularFirestore){
+
+  }
+
+  ngOnInit() {
+    this.postsCol = this.afs.collection('posts');
+    this.posts = this.postsCol.valueChanges();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule, AngularFirestore } from 'angularfire2/firestore';
+import { FormsModule } from '@angular/forms';
 
+
+  var firebaseConfig = {
+    apiKey: "AIzaSyAZJgbiQuwXaUIyx0zyiQBD5tD346nTMVg",
+    authDomain: "firestore-389db.firebaseapp.com",
+    databaseURL: "https://firestore-389db.firebaseio.com",
+    projectId: "firestore-389db",
+    storageBucket: "firestore-389db.appspot.com",
+    messagingSenderId: "184435011138"
+  };
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
